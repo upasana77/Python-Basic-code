735515683140bd424a6202806ecdc6d872939f1d
Upasana Purohit <67505447+upasana77@users.noreply.github.com>
2021-09-04 07:19:32 -0400

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,88 @@
+Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:39:15) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Last login: Fri Sep  3 10:00:25 on console
+upasanapurohit@UPASANAs-iMac-Pro ~ % 2+3
+zsh: command not found: 2+3
+upasanapurohit@UPASANAs-iMac-Pro ~ % python3
+Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:39:15) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> print("Hello World")
+Hello World
+>>> 2+3
+5
+>>> 9-8
+1
+>>> 4*6
+24
+>>> 8/4
+2.0
+>>> 5/2
+2.5
+>>> 5//2 
+2
+>>> 8+9-10
+7
+>>> 8+2*3
+14
+>>> (8+2)*3
+30
+>>> # power formula
+>>> 2 ** 3
+8
+>>> # we will use modulos operator for getting remainder in python.
+>>> 10%3
+1
+>>> use of strings
+  File "<stdin>", line 1
+    use of strings
+        ^
+SyntaxError: invalid syntax
+>>> # use of string in python, we always use them in single quote
+>>> 'navin'
+'navin'
+>>> # we can also get the output by using the inbuilt function called print, a set of code, which perform certain task.
+>>> print('navin')
+navin
+>>> # so the difference between the two output is in print stament the single quote is not getting printed. which is looking great.
+>>> print("navin's laptop")
+navin's laptop
+>>> print('navis "laptop')
+navis "laptop
+>>> print('navins \'s "laptop"')
+navins 's "laptop"
+>>> # how to print a same name multiple times.
+>>> # how to print a same name multiple times.
+>>> 'navin'+'navin'
+'navinnavin'
+>>> # we have use here concatenation,what if I want to print naveen 10 times
+>>> 10 * 'navin'
+'navinnavinnavinnavinnavinnavinnavinnavinnavinnavin'
+>>> \n means always new line in python,
+  File "<stdin>", line 1
+    \n means always new line in python,
+                                      ^
+SyntaxError: unexpected character after line continuation character
+>>> # \n means always new line in python
+>>> c: \docs
+  File "<stdin>", line 1
+    c: \docs
+           ^
+SyntaxError: unexpected character after line continuation character
+>>> # \n means new line in python
+>>> print('c:\docs\navin')
+c:\docs
+avin
+>>> # we have print the above statement in new line.
+>>> # if we want to print the string as a raw string we can print as it is.
+>>> # so here we are asking python to print the raw strings, saying don't try to covert it.
+>>> print(r'c:\docs\navin')
+c:\docs\navin
+>>> wq!
+  File "<stdin>", line 1
+    wq!
+      ^
+SyntaxError: invalid syntax
+>>> 
+
